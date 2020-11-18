ac2bc7f1a631f8ddeeef0d5ef151ed6ce7ea676c
Daniel C <56047938+dconford@users.noreply.github.com>
2020-11-18 11:52:48 -0600

    added individual course cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +10,8 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
-
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
 
 }
